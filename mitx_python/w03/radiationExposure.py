dd64547c584b5d5299788138df094cfb2c79ab1e
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-01-27 07:53:58 +0300

    W03 radiation passed.

--- mitx_python/w03/radiationExposure.py
+++ mitx_python/w03/radiationExposure.py
@@ -6,6 +6,7 @@ import unittest
 def f(x):
     return 10*math.e**(math.log(0.5)/5.27 * x)
 
+
 def radiationExposure(start, stop, step):
     '''
     Computes and returns the amount of radiation exposed
@@ -21,8 +22,20 @@ def radiationExposure(start, stop, step):
     returns: float, the amount of radiation exposed to 
       between start and stop times.
     '''
-    return 0
+    step_number = int((stop - start)/step)
+    amount = 0
+    for step_num in range(step_number):
+        amount += f(start + step_num*step)*step
+    return amount
+
 
 class TestRadiation(unittest.TestCase):
     def test_from_lecture(self):
-        self.assertEqual(radiationExposure(0, 5, 1), 39.10318784326239)
+        self.assertAlmostEqual(radiationExposure(0, 5, 1), 39.10318784326239)
+        self.assertAlmostEqual(radiationExposure(5, 11, 1), 22.94241041057671)
+        self.assertAlmostEqual(radiationExposure(0, 11, 1), 62.0455982538)
+        self.assertAlmostEqual(radiationExposure(40, 100, 1.5), 0.434612356115)
+
+
+if __name__ == '__main__':
+    unittest.main()
