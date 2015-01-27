2483aebc82fb6edb1bddda25724000927b0bcf11
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-01-27 07:44:13 +0300

    Week 3 begin

---
+++ mitx_python/w03/radiationExposure.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+
+import math
+import unittest
+
+def f(x):
+    return 10*math.e**(math.log(0.5)/5.27 * x)
+
+def radiationExposure(start, stop, step):
+    '''
+    Computes and returns the amount of radiation exposed
+    to between the start and stop times. Calls the 
+    function f (defined for you in the grading script)
+    to obtain the value of the function at any point.
+ 
+    start: integer, the time at which exposure begins
+    stop: integer, the time at which exposure ends
+    step: float, the width of each rectangle. You can assume that
+      the step size will always partition the space evenly.
+
+    returns: float, the amount of radiation exposed to 
+      between start and stop times.
+    '''
+    return 0
+
+class TestRadiation(unittest.TestCase):
+    def test_from_lecture(self):
+        self.assertEqual(radiationExposure(0, 5, 1), 39.10318784326239)
